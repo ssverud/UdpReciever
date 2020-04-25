91f48c43a8d0235a8c820587400f9dc05a9b0284
ssverud <svirpstef@gmail.com>
2020-04-25 17:05:27 +0200

    hurtig hurtig hurtig

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Drone 2 player game");
         primaryStage.setScene(new Scene(root, 600, 500));
         primaryStage.show();
     }
