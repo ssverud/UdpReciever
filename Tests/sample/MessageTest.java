af478291b8bd013c667db901f885c557ef4ec012
ssverud <svirpstef@gmail.com>
2020-04-26 11:34:25 +0200

    created tests finished

---
+++ Tests/sample/MessageTest.java
@@ -0,0 +1,27 @@
+package sample;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MessageTest {
+
+    Message message = new Message("Init");
+    String testString2 = "hej Ebbe";
+
+    @Test
+    void getMessage() {
+
+        String testString = message.getMessage();
+        assertEquals(testString, message.getMessage());
+        assertNotEquals(testString2, message.getMessage());
+    }
+
+    @Test
+    void setMessage() {
+
+        assertNotEquals(testString2, message.getMessage());
+        message.setMessage("hej Ebbe");
+        assertEquals(testString2, message.getMessage());
+    }
+}
